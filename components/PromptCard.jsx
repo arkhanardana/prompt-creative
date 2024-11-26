7e381c1b45f03dc82a14c5b85b64a57559859671
arkhanardana <hanzmantap@gmail.com>
2024-11-26 19:26:31 +0700

    add promptcard to home page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
 
-const PromptCard = () => {
-	return <div>PromptCard</div>;
+const PromptCard = ({ handleTagClick, post, handleEdit, handleDelete }) => {
+	return (
+		<div className="prompt_card">
+			<div className="flex items-start justify-between gap-5">
+				<div>
+					<Image
+						src={post.creator.image}
+						alt="user_image"
+						width={40}
+						height={40}
+						className="object-contain rounded-full"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PromptCard;
