03163703a03f4fe6770843590d91718ba0f990f1
arkhanardana <hanzmantap@gmail.com>
2024-11-27 18:32:25 +0700

    add clickable tag

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 
 const PromptCard = ({ handleTagClick, post, handleEdit, handleDelete }) => {
+	const [copied, setCopied] = useState("");
+
 	return (
 		<div className="prompt_card">
 			<div className="flex items-start justify-between gap-5">
-				<div>
+				<div className="flex items-center justify-start flex-1 gap-3 cursor-pointer">
 					<Image
 						src={post.creator.image}
 						alt="user_image"
@@ -16,8 +18,29 @@ const PromptCard = ({ handleTagClick, post, handleEdit, handleDelete }) => {
 						height={40}
 						className="object-contain rounded-full"
 					/>
+
+					<div className="flex flex-col">
+						<h3 className="font-semibold text-gray-900 font-satoshi">{post.creator.username}</h3>
+						<p className="text-sm text-gray-500 font-inter">{post.creator.email}</p>
+					</div>
+				</div>
+
+				<div className="copy_btn" onClick={() => {}}>
+					<Image
+						src={copied === post.prompt ? "/assets/icons/tick.svg" : "/assets/icons/copy.svg"}
+						width={12}
+						height={12}
+					/>
 				</div>
 			</div>
+
+			<p className="my-4 text-sm text-gray-700 font-satoshi">{post.prompt}</p>
+			<p
+				className="text-sm cursor-pointer blue_gradient font-inter"
+				onClick={() => handleTagClick && handleTagClick(post.tag)}
+			>
+				{post.tag}
+			</p>
 		</div>
 	);
 };
